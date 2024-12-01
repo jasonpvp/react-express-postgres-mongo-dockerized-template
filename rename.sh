8b0d0da56d34831a72667a971e781a5a836cac87
Jason Van Pelt <jason@Jasons-MacBook-Pro.local>
2024-11-30 21:29:16 -0800

    fix more rename

--- rename.sh
+++ rename.sh
@@ -2,21 +2,17 @@
 read -p "App name: " appName
 read -p "Root DB password: " dbRootPwd
 read -p "Web app DB Name: " appDbName
-read -p "Web app DB User: " appDbUser
 read -p "Web app DB password: " appDbPwd
 
 echo "Replacing WEB_APP_NAME with '$appName'"
-grep --exclude rename.sh -rl WEB_APP_NAME ./ | xargs sed -i '' -e s/WEB_APP_NAME/appName/g
+grep --exclude rename.sh -rl WEB_APP_NAME ./ | xargs sed -i '' -e s/WEB_APP_NAME/$appName/g
 
 echo "Replacing WEB_APP_ROOT_USER_PWD with '$dbRootPwd'"
-grep --exclude rename.sh -rl WEB_APP_ROOT_USER_PWD ./ | xargs sed -i '' -e s/WEB_APP_ROOT_USER_PWD/dbRootPwd/g
+grep --exclude rename.sh -rl WEB_APP_ROOT_USER_PWD ./ | xargs sed -i '' -e s/WEB_APP_ROOT_USER_PWD/$dbRootPwd/g
 
 echo "Replacing WEB_APP_DB_NAME with '$appDbName'"
-grep --exclude rename.sh -rl WEB_APP_DB_NAME ./ | xargs sed -i '' -e s/WEB_APP_DB_NAME/appDbName/g
-
-echo "Replacing WEB_APP_DB_USER with '$appDbUser'"
-grep --exclude rename.sh -rl WEB_APP_DB_USER ./ | xargs sed -i '' -e s/WEB_APP_DB_USER/appDbUser/g
+grep --exclude rename.sh -rl WEB_APP_DB_NAME ./ | xargs sed -i '' -e s/WEB_APP_DB_NAME/$appDbName/g
 
 echo "Replacing WEB_APP_DB_PWD with '$appDbPwd'"
-grep --exclude rename.sh -rl WEB_APP_DB_PWD ./ | xargs sed -i '' -e s/WEB_APP_DB_PWD/appDbPwd/g
+grep --exclude rename.sh -rl WEB_APP_DB_PWD ./ | xargs sed -i '' -e s/WEB_APP_DB_PWD/$appDbPwd/g
 
